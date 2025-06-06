8a89d17a7bdb2a01391f970f88d12dc20d065582
Baole795 <giabao02le@gmail.com>
2025-06-06 15:00:34 +0700

    Create new Project

---
+++ app/src/main/java/com/example/travelapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.travelapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.travelapp.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // Đúng cú pháp: Inflate layout và gán vào binding
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Thiết lập sự kiện cho nút start
+        binding.startBtn.setOnClickListener(v -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        });
+    }
+}
